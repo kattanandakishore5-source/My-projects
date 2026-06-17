1113513a7b72b9aeb1d8bdc1d9d4247aa940e066
Nanda Kishore <kattanandakishore5@gmail.com>
2026-06-17 13:04:44 +0530

    Pass request object to AuthenticationForm constructor in login_view

--- accounts/views.py
+++ accounts/views.py
@@ -67,7 +67,7 @@ def activate_account(request, uidb64, token):
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
